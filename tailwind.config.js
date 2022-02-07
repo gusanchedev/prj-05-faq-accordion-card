79aa9e84a4354d4e9b16e293de61e432e4493eb1
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-02-06 19:56:47 -0500

    📱  Classes for mobile (375px) screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kumbhSans: ["Kumbh Sans", "sans-serif"],
+      },
+      colors: {
+        veryDarkDesaturatedBlue: "hsl(238, 29%, 16%)",
+        softRed: "hsl(14, 88%, 65%)",
+        softViolet: "hsl(273, 75%, 66%)",
+        softBlue: "hsl(240, 73%, 65%)",
+        veryDarkGrayishBlue: "hsl(237, 12%, 33%)",
+        darkGrayishBlue: "hsl(240, 6%, 50%)",
+        lightGrayishBlue: "hsl(240, 5%, 91%)",
+      },
+      backgroundImage: {
+        mobile: "url('/images/bg-pattern-mobile.svg')",
+        desktop: "url('/images/bg-pattern-desktop.svg')",
+      },
+    },
   },
   plugins: [],
-}
+};
