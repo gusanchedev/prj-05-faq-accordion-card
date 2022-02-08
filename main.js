03c4f47c0853b65cc1deb0b89e258a60f474e9c2
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-02-08 09:00:16 -0500

    ✨  Add JS for managing states

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 import "./style.css";
 
 const answers = document.getElementsByClassName("answer");
-
 const questions = document.getElementsByClassName("question");
+const icons = document.getElementsByClassName("icon");
 
 for (let index = 0; index < questions.length; index++) {
-  const element = questions[index];
+  const question = questions[index];
   let clickedElement = false;
-  let visibleAnswer = false;
-  element.addEventListener("click", (e) => {
+  // let visibleAnswer = false;
+  // let displayedIcon = false;
+  question.addEventListener("click", (e) => {
     if (!clickedElement) {
       e.target.style.fontWeight = "bold";
       answers[index].style.display = "block";
+      icons[index].style.transform = "rotate(180deg)";
       clickedElement = !clickedElement;
-      visibleAnswer = !visibleAnswer;
+      // visibleAnswer = !visibleAnswer;
+      // displayedIcon = !displayedIcon;
     } else {
       e.target.style.fontWeight = "normal";
       answers[index].style.display = "none";
+      icons[index].style.transform = "rotate(360deg)";
       clickedElement = !clickedElement;
-      visibleAnswer = !visibleAnswer;
+      // visibleAnswer = !visibleAnswer;
+      // displayedIcon = !displayedIcon;
     }
   });
 }
