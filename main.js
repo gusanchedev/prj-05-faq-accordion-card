80e79369ab4f9fa8ede95b5505a94bdd55d25594
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-02-09 19:52:22 -0500

    🐛  Fix padding for answers and card location in body

--- main.js
+++ main.js
@@ -7,23 +7,20 @@ const icons = document.getElementsByClassName("icon");
 for (let index = 0; index < questions.length; index++) {
   const question = questions[index];
   let clickedElement = false;
-  // let visibleAnswer = false;
-  // let displayedIcon = false;
+
   question.addEventListener("click", (e) => {
     if (!clickedElement) {
       e.target.style.fontWeight = "bold";
       answers[index].style.display = "block";
       icons[index].style.transform = "rotate(180deg)";
       clickedElement = !clickedElement;
-      // visibleAnswer = !visibleAnswer;
-      // displayedIcon = !displayedIcon;
+
     } else {
       e.target.style.fontWeight = "normal";
       answers[index].style.display = "none";
       icons[index].style.transform = "rotate(360deg)";
       clickedElement = !clickedElement;
-      // visibleAnswer = !visibleAnswer;
-      // displayedIcon = !displayedIcon;
+
     }
   });
 }
